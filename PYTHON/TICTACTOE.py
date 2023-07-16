c4b4d7cf986a7f95bcd9c656d149b40eb0b5da7b
Selvin Desouza <110559981+Selvin-Desouza@users.noreply.github.com>
2023-07-16 20:51:09 +0530

    Update TICTACTOE.py

--- PYTHON/TICTACTOE.py
+++ PYTHON/TICTACTOE.py
@@ -12,7 +12,7 @@ matrix = np.array([0,0,0,0,0,0,0,0,0])  #initialize
 #*******************************************************************************
 print("")
 print("")
-print("]|>++++++++<||>      TICK TACK TOE       <||>++++++++<|[ \n-by SELVIN DESOUZA \n")
+print("]|>++++++++<||>----      TIC TAC TOE       ----<||>++++++++<|[ \n -by SELVIN DESOUZA					[:]--20bmiit039@gmail.com \n \n ============================================================ \n Select Positions Based on the Below given numbers :")
 print ("============================================================")
 print("Select Positions Based on the Below given numbers :")
 
@@ -27,6 +27,14 @@ print("|~~~~|>  PLAYER 1 : \"X\"    ||    PLAYER 2 : \"O\"  <|~~~~|")
 print ("============================================================")
 print("")
 #********************************************************************************
+def input_validate(N):
+    while True:
+        user_input = input(N)
+        if user_input.isdigit():
+            return int(user_input)
+        else:
+            print("Invalid input. Please enter a number.")
+
 def p1win():
     print("     =================================")
     print("     ||       PLAYER 1 WIN!!.       ||")
@@ -64,23 +72,24 @@ def printmatrix():
             displaylist.append("~")
         i=i+1
     M=''.join(displaylist)
-    print("  | 0 | 1 | 2 |   <|>   |",  M[0]  ,"|",  M[1]  ,"|",  M[2]  ,"|")
-    print("  | 3 | 4 | 5 |   <|>   |",  M[3]  ,"|",  M[4]  ,"|",  M[5]  ,"|")
-    print("  | 6 | 7 | 8 |   <|>   |",  M[6]  ,"|",  M[7]  ,"|",  M[8]  ,"|")
+    print("  | 1 | 2 | 3 |   <|>   |",  M[0]  ,"|",  M[1]  ,"|",  M[2]  ,"|")
+    print("  | 4 | 5 | 6 |   <|>   |",  M[3]  ,"|",  M[4]  ,"|",  M[5]  ,"|")
+    print("  | 7 | 8 | 9 |   <|>   |",  M[6]  ,"|",  M[7]  ,"|",  M[8]  ,"|")
     print ("============================================================")
 #********************************************************************************
 
 while i< size:
     if(i%2==0):
         print ("Player 1 >>>")
-        N=int(input("Enter the Position : "))
-        while N in list or N<0 or N>8 :
-            if(N<0 or N>8):
+        N=input_validate("Enter the Position : ")
+        while N in list or N<1 or N>9 :
+            if(N<1 or N>9):
                 print("Position out of range")
             elif N in list:
                 print("Position already selected by Player")                
-            N=int(input("Re-Enter the Position : "))
-        matrix[N] = P1
+            N=input_validate("Re-Enter the Position : ")
+        N2=N-1
+        matrix[N2] = P1
         list.append(N)
         printmatrix()
         PTN = P1
@@ -92,14 +101,15 @@ while i< size:
         i=i+1
     else:
         print ("Player 2 >>>")
-        N=int(input("Enter the Position : "))
-        while N in list or N<0 or N>8 :
-            if(N<0 or N>8):
+        N=input_validate("Enter the Position : ")
+        while N in list or N<1 or N>9 :
+            if(N<1 or N>9):
                 print("Position out of range")
             elif N in list:
                 print("Position already selected by Player")                
-            N=int(input("Re-Enter the Position : "))
-        matrix[N] = P2
+            N=input_validate("Re-Enter the Position : ")
+        N2=N-1
+        matrix[N2] = P2
         list.append(N)
         printmatrix()
         PTN = P2
